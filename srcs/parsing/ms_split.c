b7c916ea46331c7f85ef60e2a1268155b29e07ee
FateEvent <fabio.venturini.1989@gmail.com>
2022-06-23 10:51:03 +0200

    it counts the right amount of operators

--- srcs/parsing/ms_split.c
+++ srcs/parsing/ms_split.c
@@ -6,11 +6,51 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 16:57:51 by faventur          #+#    #+#             */
-/*   Updated: 2022/06/22 15:33:39 by faventur         ###   ########.fr       */
+/*   Updated: 2022/06/23 10:49:36 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+char	*ft_oper_writer_index(char *quote, char *str, int *index, int *j)
+{
+	char	oper;
+
+	oper = str[*index];
+	while (str[*index] && str[*index] == oper)
+	{
+		quote[*j] = str[*index];
+		(*j)++;
+		(*index)++;
+	}
+	quote[*j] = '\0';
+	printf("oper: %s\n", quote);
+	return (quote);
+}
+
+int	oper_len_index(char *str, char oper, int *index)
+{
+	int	counter;
+
+	counter = 0;
+	while (str[*index] && str[*index] == oper)
+	{
+		counter++;
+		(*index)++;
+	}
+	return (counter);
+}
+
+int	ft_isoper(char c)
+{
+	if (c == '<')
+		return (1);
+	if (c == '|')
+		return (2);
+	if (c == '>')
+		return (3);
+	return (0);
+}
 
 int	ms_check_charset(char c)
 {
@@ -32,16 +72,27 @@ static int	wordgroup_counter(char *s)
 			counter += between_squotes(s, &i);
 		else if (s[i] == '\"')
 			counter += between_dquotes(s, &i);
-		else if (!ms_check_charset(s[i]) && ms_check_charset((s[i + 1])))
+		else if (ft_isoper(s[i]) && !ft_isoper(s[i + 1]))
+		{
 			counter++;
+			printf("count: %c %d\n", s[i], counter);
+		}
+		else if (!ms_check_charset(s[i]) && (ms_check_charset(s[i + 1])
+				|| ft_isoper(s[i + 1])))
+		{
+			counter++;
+			printf("je rentre ici");
+		}
 		i++;
 	}
+	printf("wgcounter: %d\n", counter);
 	return (counter);
 }
 
 static int	ms_let_count(char *str, int *index)
 {
-	int	counter;
+	int		counter;
+	char	oper;
 
 	counter = 0;
 	while (ms_check_charset(str[*index]))
@@ -52,6 +103,11 @@ static int	ms_let_count(char *str, int *index)
 			counter += squote_len_index(str, index);
 		else if (str[*index] == '\"')
 			counter += dquote_len_index(str, index);
+		else if (ft_isoper(str[*index]))
+		{
+			oper = str[*index];
+			counter += oper_len_index(str, oper, index);
+		}
 		else
 		{
 			(*index)++;
@@ -74,6 +130,8 @@ static char	*wordgroup_split(char *newstr, char *str, int *index)
 			ft_squote_pruner_index(newstr, str, index, &j);
 		else if (str[*index] == '\"')
 			ft_dquote_pruner_index(newstr, str, index, &j);
+		else if (str[*index] == '\"')
+			ft_oper_writer_index(newstr, str, index, &j);
 		else
 			newstr[j++] = str[(*index)++];
 	}
