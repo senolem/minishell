b27130553c95a212fe24dc75ab8cab117472f0b0
FateEvent <fabio.venturini.1989@gmail.com>
2022-06-25 17:50:00 +0200

    split

--- srcs/parsing/double_quotes.c
+++ srcs/parsing/double_quotes.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:46:13 by faventur          #+#    #+#             */
-/*   Updated: 2022/06/23 14:38:05 by faventur         ###   ########.fr       */
+/*   Updated: 2022/06/25 17:49:09 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 int	between_dquotes(char *str, int *index)
 {
 	int	counter;
+	int	char_counter;
 
 	counter = 0;
+	char_counter = 0;
 	if (str[*index] == '\"')
 		(*index)++;
 	while (str[*index])
@@ -24,12 +26,14 @@ int	between_dquotes(char *str, int *index)
 		if (str[*index] == '\"' && (ft_isoper(str[*index + 1])
 				|| ms_check_charset(str[*index + 1])))
 		{
-			counter++;
-			return (counter);
+			if (char_counter > 0)
+				counter++;
+			break ;
 		}
 		else if (str[*index] == '\"' && !ms_check_charset(str[*index + 1]))
 			break ;
 		(*index)++;
+		char_counter++;
 	}
 	return (counter);
 }
--- srcs/parsing/ft_isoper.c
+++ srcs/parsing/ft_isoper.c
@@ -3,28 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isoper.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
+/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:57:36 by albaur            #+#    #+#             */
-/*   Updated: 2022/06/23 14:36:02 by albaur           ###   ########.fr       */
+/*   Updated: 2022/06/25 17:19:39 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*ft_oper_writer_index(char *quote, char *str, int *index, int *j)
+int	ft_isoper(char c)
 {
-	char	oper;
-
-	oper = str[*index];
-	while (str[*index] && str[*index] == oper)
-	{
-		quote[*j] = str[*index];
-		(*j)++;
-		(*index)++;
-	}
-	quote[*j] = '\0';
-	return (quote);
+	if (c == '<')
+		return (1);
+	if (c == '|')
+		return (2);
+	if (c == '>')
+		return (3);
+	if (c == '&')
+		return (4);
+	return (0);
 }
 
 int	oper_len_index(char *str, int *index)
@@ -42,13 +40,18 @@ int	oper_len_index(char *str, int *index)
 	return (counter);
 }
 
-int	ft_isoper(char c)
+char	*ft_oper_writer_index(char *quote, char *str, int *index, int *j)
 {
-	if (c == '<')
-		return (1);
-	if (c == '|')
-		return (2);
-	if (c == '>')
-		return (3);
-	return (0);
+	char	oper;
+
+	oper = str[*index];
+	while (str[*index] && str[*index] == oper)
+	{
+		quote[*j] = str[*index];
+		(*j)++;
+		(*index)++;
+	}
+	quote[*j] = '\0';
+	return (quote);
 }
+
