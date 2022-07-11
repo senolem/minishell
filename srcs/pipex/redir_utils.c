71a01c3bd5ccdf349b645b218bc3120847a08ec1
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-11 16:51:41 +0200

    redirecting

--- srcs/pipex/redir_utils.c
+++ srcs/pipex/redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:22:24 by faventur          #+#    #+#             */
-/*   Updated: 2022/07/11 16:40:46 by faventur         ###   ########.fr       */
+/*   Updated: 2022/07/11 16:51:24 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 /*
 void	ft_dst_manager(char **av, size_t *index)
 {
-	;
-}
-*/
-void	ft_gt_manager(t_stack **av, size_t *index)
-{
-	;
+	var->fd[0] = open(av[1], O_RDONLY);
+	if (var.fd[0] < 0)
+		ft_printerror("pipex", av[1]);
 }
 
-void	ft_redir_parser(t_stack **av)
+void	ft_gt_manager(t_stack **av, t_var *var, size_t *index)
+{
+	var->fd[1] = open(av[ac - 1], O_WRONLY | O_CREAT, 0644);
+	if (var.fd[1] < 0)
+		ft_printerror("pipex", av[ac - 1]);
+}
+
+void	ft_redir_parser(t_stack **av, t_var *var)
 {
 	size_t	i;
 
@@ -30,7 +34,7 @@ void	ft_redir_parser(t_stack **av)
 	while (av[i])
 	{
 		if (ft_strnstrbool(av[i], ">", 1))
-			ft_gt_manager(av, &i);
+			ft_gt_manager(av, var, &i);
 //		else if (!ft_strnstrbool(av[i], "<", 1))
 //			ft_dst_manager(av, &i);
 //		else if (!ft_strnstrbool(av[i], ">>", 2))
@@ -40,3 +44,4 @@ void	ft_redir_parser(t_stack **av)
 		i++;
 	}
 }
+*/
