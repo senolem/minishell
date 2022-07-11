cc50a05fed055b6a6e75ae16ea64f945ddcb199e
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-11 11:33:00 +0200

    en commentaire de nouveau

--- srcs/pipex/pipe_utils.c
+++ srcs/pipex/pipe_utils.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 16:48:03 by faventur          #+#    #+#             */
-/*   Updated: 2022/07/11 11:32:16 by faventur         ###   ########.fr       */
+/*   Updated: 2022/07/11 11:32:50 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,4 @@ size_t	pipe_counter(t_stack *stack)
 		node = node->next;
 	}
 	return (i);
-}
-
-int	parg_size(char *str, size_t i)
-{
-	int	counter;
-
-	counter = 0;
-	while (str[i])
-	{
-		if (str[i - 1] != '|' && str[i] == '|' && str[i + 1] != '|')
-			break ;
-		++counter;
-		++i;
-	}
-	++counter;
-	return (counter);
 }
