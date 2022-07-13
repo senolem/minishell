d7194c6e4037759feaa34e723f3b3488d47fc09d
albaur <albaur@student.42.fr>
2022-07-13 15:37:14 +0200

    :)

--- srcs/parsing/process_input.c
+++ srcs/parsing/process_input.c
@@ -6,24 +6,42 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:20:12 by albaur            #+#    #+#             */
-/*   Updated: 2022/07/13 15:27:11 by albaur           ###   ########.fr       */
+/*   Updated: 2022/07/13 15:36:31 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	check_input(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	if (str[0] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (ft_isascii(str[i]) == 1 && ft_isspace(str[i]) == 0)
+			return (1);
+		++i;
+	}
+	return (0);
+}
+
 void	process_input(t_data *data)
 {
 	char	**arr;
 
-	if (data->input && data->input[0] != '\0' && data->mode)
+	if (check_input(data->input) == 1)
 		add_history(data->input);
 	if (ft_check_quotes(data->input) > 0)
 	{
 		ft_printf("\033[31mSyntax error : invalid quote sequence\033[0m\n");
 		return ;
 	}
-	if (data->input && data->input[0] != '\0')
+	if (check_input(data->input) == 1)
 	{
 		arr = ms_split(data->input);
 		if (arr)
