59c65a666920fdb9b151058a1851a6436289eaae
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-19 14:55:32 +0200

    look

--- srcs/pipex/pipe_manager.c
+++ srcs/pipex/pipe_manager.c
@@ -6,11 +6,13 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 21:43:57 by faventur          #+#    #+#             */
-/*   Updated: 2022/07/19 14:39:26 by faventur         ###   ########.fr       */
+/*   Updated: 2022/07/19 14:49:14 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+
 
 void	ft_last_action(t_var var, int ac, char *av[])
 {
