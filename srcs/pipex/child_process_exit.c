b2afcded78217fafef06218e3e8ccb5757a6caa1
albaur <albaur@student.42.fr>
2022-07-20 12:31:56 +0200

    freeing exit ft

--- srcs/pipex/child_process_exit.c
+++ srcs/pipex/child_process_exit.c
@@ -6,7 +6,7 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 11:25:59 by albaur            #+#    #+#             */
-/*   Updated: 2022/07/20 12:19:11 by albaur           ###   ########.fr       */
+/*   Updated: 2022/07/20 12:30:56 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,22 @@ void	child_process_exit(int code)
 	int		shlvl;
 	char	*tmp;
 	char	*err;
+	int		ret;
 
 	if (ft_atoi(env_get_arg("?EXIT")) != 0)
 	{
 		shlvl = env_get_shlvl();
 		tmp = ft_itoa(shlvl - 1);
+		err = env_get_arg("?EXIT");
+		ret = ft_atoi(err);
 		env_set_arg("SHLVL", tmp);
 		env_set_arg("?", ft_itoa(WEXITSTATUS(code)));
-		err = env_get_arg("?EXIT");
 		env_set_arg("?EXIT", "0");
 		if (shlvl == 1)
 			unlink(ENV_FILE);
-		exit(ft_atoi(err));
+		free(tmp);
+		free(err);
+		exit(ret);
 	}
 	else
 	{
