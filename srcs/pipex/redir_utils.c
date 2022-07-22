41d222c9d260cbc11ebb8832eb4bdab22fb67dfb
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-22 11:41:19 +0200

    here_doc

--- srcs/pipex/redir_utils.c
+++ srcs/pipex/redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 12:19:04 by albaur            #+#    #+#             */
-/*   Updated: 2022/07/20 17:22:13 by faventur         ###   ########.fr       */
+/*   Updated: 2022/07/22 11:32:23 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	redir_fd(t_stack **av, t_var *var, t_redir i)
 			path = ft_lst_to_arrdup(node->content);
 			var->fd[i.fd] = open(path, i.mode, 0644);
 			if (var->fd[i.fd] < 0)
-				ret_err(strerror(errno), NULL, 0);
+				return (ret_err(strerror(errno), NULL, 0));
 			redir_clear(node->prev, av);
 			redir_clear(node, av);
 			free(path);
