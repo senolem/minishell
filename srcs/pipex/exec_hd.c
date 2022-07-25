70185a2c4e75ebdce67d735359502f1415462091
Alexandre Baur <senolem22@gmail.com>
2022-07-25 15:32:14 +0200

    fixed hd again again again

--- srcs/pipex/exec_hd.c
+++ srcs/pipex/exec_hd.c
@@ -6,7 +6,7 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 14:22:20 by albaur            #+#    #+#             */
-/*   Updated: 2022/07/25 14:28:58 by albaur           ###   ########.fr       */
+/*   Updated: 2022/07/25 15:26:39 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ void	exec_hd(char *path, t_hd *hd)
 	pid_t	pid;
 	int		code;
 
-	code = 0;
+	hd->code = 0;
 	pid = fork();
+	sig_toggle(2);
 	if (pid < 0)
 		return (ret_null("fork:", strerror(errno)));
 	if (pid == 0)
@@ -26,11 +27,15 @@ void	exec_hd(char *path, t_hd *hd)
 		sig_toggle(3);
 		ft_hd_performer(path, hd);
 		sig_toggle(0);
-		exit(0);
 	}
 	else if (pid > 0)
 	{
 		rl_on_new_line();
 		waitpid(pid, &code, 0);
 	}
+	if (WIFEXITED(code))
+		hd->code = WEXITSTATUS(code);
+	if (hd->code == 1)
+		env_set_arg("?", "1");
+	sig_toggle(0);
 }
